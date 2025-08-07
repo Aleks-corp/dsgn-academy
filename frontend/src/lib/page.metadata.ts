ef94c5d8690a715d270dbc83abd3086f8aa88679
Aleks-corp <nporock@gmail.com>
2025-08-07 20:40:52 +0300

    fix meta

--- frontend/src/lib/page.metadata.ts
+++ frontend/src/lib/page.metadata.ts
@@ -3,6 +3,16 @@ import type { Metadata } from "next";
 import type { ICourse } from "@/types/courses.type";
 import { IVideo } from "@/types/videos.type";
 
+function isVideo(obj: ICourse | IVideo): obj is IVideo {
+  return (
+    (obj as IVideo).video !== undefined && (obj as IVideo).cover !== undefined
+  );
+}
+
+function isCourse(obj: ICourse | IVideo): obj is ICourse {
+  return (obj as ICourse).videos !== undefined;
+}
+
 export async function getSeoMetadata(
   type: "course" | "video",
   id: string
@@ -35,25 +45,30 @@ export async function getSeoMetadata(
     };
   }
 
+  let cover: string | undefined = undefined;
+  if (isVideo(item)) {
+    cover = item.cover;
+  } else if (isCourse(item)) {
+    cover = item.videos?.[0]?.cover;
+  }
+
+  const description =
+    item.description?.slice(0, 160) ||
+    "Детальний опис для дизайнерів на Dsgn Academy.";
+
   return {
     title: `${item.title} | Dsgn Academy`,
-    description:
-      item.description?.slice(0, 160) ||
-      "Детальний опис для дизайнерів на Dsgn Academy.",
+    description,
     openGraph: {
       title: `${item.title} | Dsgn Academy`,
-      description:
-        item.description?.slice(0, 160) ||
-        "Детальний опис для дизайнерів на Dsgn Academy.",
-      images: item.cover ? [{ url: item.cover, width: 1200, height: 630 }] : [],
+      description,
+      images: cover ? [{ url: cover, width: 1200, height: 630 }] : [],
     },
     twitter: {
       card: "summary_large_image",
       title: `${item.title} | Dsgn Academy`,
-      description:
-        item.description?.slice(0, 160) ||
-        "Детальний опис для дизайнерів на Dsgn Academy.",
-      images: item.cover ? [item.cover] : [],
+      description,
+      images: cover ? [cover] : [],
     },
   };
 }
