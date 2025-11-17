5f66035cba0dbadbd33e9edccd4e22e8fa77780f
Aleks-corp <nporock@gmail.com>
2025-11-17 12:25:05 +0200

    add new stream data

--- backend/src/types/stream.type.ts
+++ backend/src/types/stream.type.ts
@@ -3,6 +3,7 @@ import type { Document, Types } from "mongoose";
 export interface IStream extends Document {
   _id: Types.ObjectId;
   title: string;
+  description: string;
   videoId: string;
   startShowBannerAt: Date;
   startStreamAt: Date;
